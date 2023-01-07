0a3c9bfe06d0ff96d2839470c60422b663c2f3df
Basheeth <basheethsheik@gmail.com>
2023-01-07 14:43:18 +0530

    starter code

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+// function  to create the quiz
+createQuiz();
+
+
+
+function createQuiz(){
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let i =data[Math.floor(Math.random()*data.length)];
+        console.log(i);
+        console.log(i.name);
+    })
+}
