06b11d43f80ecbbc23abac19b5aa1766cbfab6a0
Basheeth <basheethsheik@gmail.com>
2023-01-07 15:10:24 +0530

    do option

--- index.js
+++ index.js
@@ -8,16 +8,38 @@ createQuiz();
 
 function createQuiz(){
 
+    let option1 = [];
+    let option2 = [];
     fetch("https://restcountries.com/v3.1/all")
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        let i = data[Math.floor(Math.random()*data.length)];
-        questionType = Math.floor(Math.random()*3);
-        buildQuestion(i.name.common)
-        console.log(i.name.common);
-        console.log(i.capital[0]);
-        console.log(i.flag);
+        let country = data[Math.floor(Math.random()*data.length)];
+        for (let i = 0 ; i < 3 ; i++){
+            let fakeCountries = data[Math.floor(Math.random()*3)];
+            
+        }
+        buildQuestion(country.name.common,country.capital[0],country.flag);
     })
+}
+
+function buildQuestion(name,capital,flag){
+    questionType = Math.floor(Math.random()*3);
+
+    if (questionType == 1){
+        rendorHtml(`What is the capital of${b}`)
+    }
+    else if (questionType == 2){
+        rendorHtml(`${capital} is the capital of `)
+    }
+    else{
+        rendorHtml(`${flag} which country does this flag belongs to?`)
+    }
+
+}
+
+
+function rendorHtml(){
+
 }
